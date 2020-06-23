7ec016d3f5b8d76ea4b64069a93e27273d9c41d5
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2020-06-23 20:37:07 +1200

    Changed to using read_fwf to handle gross tables

--- cluster_price.py
+++ cluster_price.py
@@ -19,63 +19,15 @@ gibikibi = 1048576  # One GiBibyte in KibiBytes
 mibikibi = 1024  #one MibiByte in Kibibytes
 gibimibi = 1024 # one gibibyte in mibibytes
 
-def clean_slurm1(mystr):
-    mystr = re.sub(' +', ',',mystr)
-    users_list = mystr.split('\n')
-    users_list.pop(1)  #remove ------
-    users_list.pop(-1) #remove empty end line
-
-    clean_user_list = [mystring[1:] if mystring[0]==',' else mystring for mystring in users_list ] #all users except the longest username will have a leading ',' at this point, remove that 
-    mystr = '\n'.join(clean_user_list) # recreate the now clean string
-    return mystr
-
-def clean_slurm2(mystr):
-    #prepare output from sacct initial:
-    # 2979             andre   00:00:10          8                       
-    # 2979.batch               00:00:10          8      1508K    157460K 
-    # 2979.extern              00:00:10          8      1128K    156940K 
-    # 2979.0                   00:00:09          8      1148K    225560K 
-    # 3247             andre   00:00:00         16                       
-    # --> grep removes .ext* lines -- uneeded, just a result of using cgroups
-    # --> replace . with spaces
-    # 2979             andre   00:00:10          8                       
-    # 2979 batch               00:00:10          8      1508K    157460K 
-    # 2979 0                   00:00:09          8      1148K    225560K 
-    # 3247             andre   00:00:00         16         
-    # --> remove extra white space
-    # 2979 andre 00:00:10 8                       
-    # 2979 batch 00:00:10 8 1508K 157460K 
-    # 2979 0 00:00:09 8 1148K 225560K 
-    # 3247 andre 00:00:00 16
-    # so when it goes into a pandas dataframe:
-    # 2979 | andre | 00:00:10 | 8 |       |                       
-    # 2979 | batch | 00:00:10 | 8 | 1508K | 157460K 
-    # 2979 | 0     | 00:00:09 | 8 | 1148K | 225560K 
-    # 3247 | andre | 00:00:00 | 16|       |
-    mystr.replace('.',' ',1)
-    mystr = re.sub(' +', ',',mystr)
-    jobs_list = mystr.split('\n')
-    jobs_list.pop(1)  #remove ------
-
-    mystr = '\n'.join(jobs_list) # recreate the now clean string
-    return jobs_list
-
-
 def user_usage(user,startdate):
     print(user)
     #get user's assigned group - we have to do via os as currently all slurm users run as user
     group_string = subprocess.run(['groups',user],stdout=subprocess.PIPE).stdout.decode('utf-8')
     user_group = group_string.strip('\n').split(' ')[-1]
-    sacct_string = subprocess.run(['sacct -S ' + startdate + ' --format="jobid%30,Elapsed,Start,NCPUS,MaxRSS,MaxVMSize,Partition,ReqCPUS,AllocCPUS,TotalCPU,ReqMem" -u '+user + '|grep -v ext'],shell=True,stdout=subprocess.PIPE).stdout.decode('utf-8')
-    saact_string_orig = sacct_string
-    sacct_string = clean_slurm2(sacct_string)
-    sacct_string = list(filter(lambda x: 'K' in x or 'M' in x or 'G' in x, sacct_string)) #remove oostinto's weird empty job strings 
-
-    sacct_string = '\n'.join(sacct_string)
-
-    sacctio = StringIO(sacct_string)
-    sacctdf = pd.read_csv(sacctio)
-    df = sacctdf
+    sacct_string = subprocess.run(['sacct -S ' + startdate + ' --format="jobid%30,Elapsed,Start,NCPUS,MaxRSS,MaxVMSize,Partition,ReqCPUS,AllocCPUS,TotalCPU,ReqMem,State,End" -u '+user + '|grep -v ext'],shell=True,stdout=subprocess.PIPE).stdout.decode('utf-8')
+    sacct_stringio=StringIO(sacct_string)
+    df=pd.read_fwf(sacct_stringio)
+    df=df.drop(df.index[0]) #remove all the ------ ----- -----
 
     df = df[df.MaxVMSize.notna()] #Drop NaN value MaxVMSize, which is extraneous output
     costs = []
@@ -86,7 +38,7 @@ def user_usage(user,startdate):
 
     #bad iterating over a df, TODO make better
     for row in df.itertuples():
-        cpu_request = row.NCPUS
+        cpu_request = int(row.NCPUS)
         if 'G' in row.MaxVMSize:
             memory_request = float(row.MaxVMSize.strip('G'))
         if 'M' in row.MaxVMSize:
@@ -157,35 +109,43 @@ def user_usage(user,startdate):
         mem_request_list=[np.nan] 
 
     numjobs = len(cpu_hours)
-    if numjobs==0:
-        alljobs_data = {'user':[user],
-                'group': [user_group],
-                'starttime':[np.nan],
-                'cpu_hours': [np.nan],
-                'gib_hours': [np.nan],
-                'runtime_hours':[np.nan],
-                'cpu_request':[np.nan],
-                'gib_request':[np.nan],
-                'aws_cost':[np.nan]}
-    else:   
-        alljobs_data = {'user':[user]*numjobs,
-                        'group': [user_group]*numjobs,
-                        'starttime':start_time_list,
-                        'cpu_hours': cpu_hours,
-                        'gib_hours': gib_hours,
-                        'runtime_hours':time_taken_hours,
-                        'cpu_request':cpu_request_list,
-                        'gib_request':mem_request_list,
-                        'aws_cost':costs}
-
-    return [alljobs_data, saact_string_orig]
+    # if numjobs==0:
+    #     alljobs_data = {'user':[user],
+    #             'group': [user_group],
+    #             'starttime':[np.nan],
+    #             'cpu_hours': [np.nan],
+    #             'gib_hours': [np.nan],
+    #             'runtime_hours':[np.nan],
+    #             'cpu_request':[np.nan],
+    #             'gib_request':[np.nan],
+    #             'aws_cost':[np.nan]}
+    # else:   
+    #     alljobs_data = {'user':[user]*numjobs,
+    #                     'group': [user_group]*numjobs,
+    #                     'cpu_hours': cpu_hours,
+    #                     'gib_hours': gib_hours,
+    #                     'runtime_hours':time_taken_hours,
+    #                     'cpu_request':cpu_request_list,
+    #                     'gib_request':mem_request_list,
+    #                     'aws_cost':costs}
+    df['user']=user
+    df['group']=user_group
+    df['starttime']=start_time_list
+    df['cpu_hours']=cpu_hours
+    df['gib_hours']=gib_hours
+    df['runtime_hours']=time_taken_hours
+    df['cpu_request']=cpu_request_list
+    df['gib_request']=mem_request_list
+    df['aws_cost']=costs
+    return [df]
 
 
 users_string = subprocess.run(['sacctmgr','show','user'],stdout=subprocess.PIPE).stdout.decode('utf-8')
-users_string = clean_slurm1(users_string)
-
 usersio=StringIO(users_string)
-usersdf=pd.read_csv(usersio)
+usersdf=pd.read_fwf(usersio)
+usersdf=usersdf.drop(usersdf.index[0]) #remove all the ------ ----- -----
+#usersdf=pd.read_csv(usersio)
+
 
 usernames=list(usersdf.User)
 
@@ -202,23 +162,19 @@ if use_currentdate == True:
 
     #load the allstrings as a dataframe, this is really inefficient, but makes deduping easy
     # old_allstrings = pd.read_csv('allstrings.txt',sep='\t') #1 column, with each row of data
-    old_allstrings=pd.read_csv('all_strings.csv')
+    # old_allstrings=pd.read_csv('all_strings.csv')
 
 for user in usernames:
     if user not in ['leinma']:  #TODO!  This seems tricky to solve, removed users still show up in "sacctmgr show user" but not in "sacct -S 2019-01-01 --format="jobid%30,Elapsed,Start,NCPUS,MaxRSS,MaxVMSize" -u leinma"
-        [all_jobs, saact_string_orig] = user_usage(user, startdate) 
-        all_jobs_f = pd.DataFrame.from_dict(all_jobs)
+        [all_jobs_f] = user_usage(user, startdate) 
         all_jobs_df  = pd.concat([all_jobs_df, all_jobs_f ],sort=False)
-        all_strings = all_strings + saact_string_orig
 
-# Convert all_strings to pd.dataframe to make de duping easier
-stringdata = StringIO(all_strings)
-all_strings_df = pd.read_csv(stringdata,sep='\t')
+# # Convert all_strings to pd.dataframe to make de duping easier
+# stringdata = StringIO(all_strings)
+# all_strings_df = pd.read_csv(stringdata,sep='\t')
 
 if use_currentdate == True:  #concat old and new df, removing duplicates
     all_jobs_df = pd.concat([old_df,all_jobs_df],sort=False).drop_duplicates().reset_index(drop=True)
-    all_strings_df = pd.concat([old_allstrings, all_strings_df],sort=False).drop_duplicates().reset_index(drop=True) 
 all_jobs_df.to_csv('all_jobs.csv')
-all_strings_df.to_csv('all_strings.csv')
 # with open("allstrings.txt", "w") as text_file:
 #     print(f"{all_strings}", file=text_file)
