13a71d19ca9f400271a2bd625e77e5eb2ccf41fd
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2020-09-08 12:52:41 +1200

    Modifed script location

--- update_data.sh
+++ update_data.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -l
-cd /home/andre/metrics/cluster_price
+cd /home/andre/metrics/raapoi_metrics
+
 
 # module load python/3.7.3
-/nfs/home/andre/metrics/cluster_price/env/bin/python /nfs/home/andre/metrics/cluster_price/cluster_price.py
+/nfs/home/andre/metrics/raapoi_metrics/env/bin/python /nfs//home/andre/metrics/raapoi_metrics/cluster_price.py
 
 # copy data
 cp *.csv /nfs/scratch/admin/metrics_data/
