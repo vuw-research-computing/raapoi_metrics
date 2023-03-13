84b7f024a70ff187f58299a28ce87a545c7181c0
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2023-03-14 12:45:46 +1300

    Send a list of users to syncing

--- update_data.sh
+++ update_data.sh
@@ -3,7 +3,7 @@ cd /home/andre/metrics/raapoi_metrics
 
 
 # module load python/3.7.3
-/nfs/home/andre/metrics/raapoi_metrics/env/bin/python /nfs//home/andre/metrics/raapoi_metrics/cluster_price.py
+/nfs/home/andre/metrics/raapoi_metrics/env/bin/python /nfs/home/andre/metrics/raapoi_metrics/cluster_price.py
 
 # copy data
 cp *.csv /nfs/scratch/admin/metrics_data/
