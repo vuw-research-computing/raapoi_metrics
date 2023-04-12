c660c940e6ff041e3d105dfebcb34f2df4b1e577
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2023-04-12 16:29:58 +1200

    temp change to update script to not copy date to onedrive

--- update_data.sh
+++ update_data.sh
@@ -3,16 +3,16 @@ cd /home/andre/metrics/raapoi_metrics
 
 
 # module load python/3.7.3
-/nfs/home/andre/metrics/raapoi_metrics/env/bin/python /nfs/home/andre/metrics/raapoi_metrics/cluster_price.py
-
+#/nfs/home/andre/metrics/raapoi_metrics/env/bin/python /nfs/home/andre/metrics/raapoi_metrics/cluster_price.py
+/nfs/home/andre/metrics/raapoi_metrics/env/bin/python /nfs/scratch/andre/cluster_managment/test_metrics/raapoi_metrics/cluster_price3.py
 # copy data
-cp *.csv /nfs/scratch/admin/metrics_data/
+# cp *.csv /nfs/scratch/admin/metrics_data/
 
-# Push data to onedrive for sharing
-/home/software/apps/rclone/1.54.1/rclone --config /home/andre/.config/rclone/rclone.conf copy --progress --transfers 8 /nfs/scratch/admin/metrics_data/all_jobs_new_calc.csv hdrive:/raapoi_metrics/
+# # Push data to onedrive for sharing
+# /home/software/apps/rclone/1.54.1/rclone --config /home/andre/.config/rclone/rclone.conf copy --progress --transfers 8 /nfs/scratch/admin/metrics_data/all_jobs_new_calc.csv hdrive:/raapoi_metrics/
 
-# Push data to onedrive for sharing
-/home/software/apps/rclone/1.54.1/rclone --config /home/andre/.config/rclone/rclone.conf copy --progress --transfers 8 /nfs/scratch/admin/metrics_data/userdates.txt hdrive:/raapoi_metrics/
+# # Push data to onedrive for sharing
+# /home/software/apps/rclone/1.54.1/rclone --config /home/andre/.config/rclone/rclone.conf copy --progress --transfers 8 /nfs/scratch/admin/metrics_data/userdates.txt hdrive:/raapoi_metrics/
 
 
-/home/software/apps/rclone/1.54.1/rclone --config /home/andre/.config/rclone/rclone.conf copy --progress --transfers 8 /nfs/scratch/admin/metrics_data/users.csv hdrive:/raapoi_metrics/
+# /home/software/apps/rclone/1.54.1/rclone --config /home/andre/.config/rclone/rclone.conf copy --progress --transfers 8 /nfs/scratch/admin/metrics_data/users.csv hdrive:/raapoi_metrics/
