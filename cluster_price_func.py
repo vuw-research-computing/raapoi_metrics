5c01abb598fa4141f83207ac0126b0315b6557b8
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2023-07-02 20:13:58 +1200

    Update functions

---
+++ cluster_price_func.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np
+
+def memfix(inmem):
+    if pd.isnull(inmem):
+        outmem = 0
+    else:
+        outmem = float(inmem.strip('M'))
+    return outmem
+
+
+def timeformat_lambda(timein):
+    if pd.isnull(timein):
+        timeout = '00 days 00:00:00'
+    elif timein.count('-') == 1:
+        timeout = timein.replace('-', ' days ')
+    elif timein.count(':') == 1:
+        timeout = '00 days 00:' + timein
+    elif timein.count(':') == 2:
+        timeout = '00 days ' + timein
+    else:
+        print(timein)
+        raise Exception("Invalid time format")
+    timeout = pd.to_timedelta(timeout)
+    return timeout
+
+def cleanjobid(jobid):
+    jobid = str(jobid)
+    jobid = jobid.split('.', 1)[0]
+    return jobid
+
+
+def collate_saact(jobs_data):
+    # Preprocessing steps from collate_saact function
+    column_names = list(jobs_data.columns)
+    df = pd.DataFrame(columns=column_names)
+    rootid = 'xx'
+    rowkeep = df[:1]
+
+    # Preprocessing steps from user_usage function
+    # use the loc accessor to modify the DataFrame in place
+    jobs_data.loc[:, 'MaxRSS'] = jobs_data['MaxRSS'].map(lambda x: memfix(x))
+    jobs_data.loc[:, 'MaxVMSize'] = jobs_data['MaxVMSize'].map(lambda x: memfix(x))
+
+    jobs_data.loc[:, 'Elapsed'] = jobs_data['Elapsed'].map(lambda x: timeformat_lambda(x))
+    jobs_data.loc[:, 'Timelimit'] = jobs_data['Timelimit'].map(lambda x: timeformat_lambda(x))
+    jobs_data.loc[:, 'TotalCPU'] = jobs_data['TotalCPU'].map(lambda x: timeformat_lambda(x))
+
+    jobs_data.loc[:, 'JobID'] = jobs_data['JobID'].map(lambda x: cleanjobid(x))
+
+
+    # Aggregating and processing as in the original collate_saact function
+    df_agg = jobs_data.groupby('JobID').agg({
+        'User': lambda x: x.iloc[0],
+        'Account': lambda x: x.iloc[0],
+        'JobID': lambda x: x.iloc[0],
+        'Elapsed': np.max,
+        'Timelimit': np.max,
+        'Start': lambda x: x.iloc[0],
+        'NNodes': lambda x: x.iloc[0],
+        'NTasks': np.max,
+        'MaxRSS': np.max,
+        'MaxVMSize': np.max,
+        'Partition': lambda x: x.iloc[0],
+        'ReqCPUS': lambda x: x.iloc[0],
+        'AllocCPUS': lambda x: x.iloc[0],
+        'TotalCPU': np.max,
+        'ReqMem': lambda x: x.iloc[0],
+        'AllocGRES': lambda x: x.iloc[0],
+        'State': lambda x: x.iloc[0],
+        'End': lambda x: x.iloc[0]
+    })
+
+    return jobs_data, df_agg
