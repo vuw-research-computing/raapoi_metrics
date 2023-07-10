11f4e1ec73d27c142ef7f0d44cba12e8940456ae
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2023-07-11 10:23:43 +1200

    Added the correct plot functions

--- quickplots.py
+++ quickplots.py
@@ -5,8 +5,8 @@ from functions.raapoi_plots import preprocess_data, plot_unique_users_per_month,
 def main():
     df = pd.read_csv('dfout_all.csv', dtype={15: str})
     df = preprocess_data(df)
-    plot_data_month(df)
-    plot_data_year(df)
+    plot_unique_users_per_month(df)
+    plot_unique_users_per_year(df)
 
 if __name__ == "__main__":
     main()
