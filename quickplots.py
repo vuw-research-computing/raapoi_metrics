c03f4d9d657922b1e6f33e053631e9deaf64878d
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2023-07-10 13:36:56 +1200

    Quick python file for testing plotting before merging into one program

---
+++ quickplots.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from functions.raapoi_plots import preprocess_data plot_unique_users_per_month plot_unique_users_per_year
+
+
+def main():
+    df = pd.read_csv('dfout_all.csv', dtype={15: str})
+    df = preprocess_data(df)
+    plot_data_month(df)
+    plot_data_year(df)
+
+if __name__ == "__main__":
+    main()
