3011e5188fa16f336ed80a1dce63b6bcb39a4ff8
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2023-07-12 10:56:38 +1200

    Added check for extra incomplete months

--- cluster_price5.py
+++ cluster_price5.py
@@ -63,6 +63,17 @@ csv_files = sorted([file for file in os.listdir(directory) if file.endswith(".cs
 
 start_time = time.time()
 
+# Check for any "complete" files and remove corresponding "incomplete" files
+for csv_file in csv_files:
+    month_str, status = csv_file.split("_")[:2]
+    if status == "complete":
+        incomplete_file_path = os.path.join(directory, f"{month_str}_incomplete.csv")
+        if os.path.exists(incomplete_file_path):
+            os.remove(incomplete_file_path)
+            csv_files.remove(f"{month_str}_incomplete.csv")  # Remove the file from our list too
+            print(f"Removed file: {incomplete_file_path}")
+
+# Now proceed to join files
 with open(output_file, "w") as outfile:
     for index, csv_file in enumerate(csv_files):
         if csv_file == "all_data.csv":
