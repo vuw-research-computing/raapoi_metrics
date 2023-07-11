47a708f490dbc462313326b74f04c51d71d085c7
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2023-07-11 12:24:46 +1200

    Lets get all plots

--- quickplots.py
+++ quickplots.py
@@ -1,13 +1,13 @@
 import pandas as pd
-from functions.raapoi_plots import preprocess_data, plot_unique_users_per_month, plot_unique_users_per_year, plot_costs_per_year
+from functions.raapoi_plots import preprocess_data, plot_unique_users_per_month, plot_unique_users_per_year, plot_costs_per_year, plot_costs_per_month
 
 
 def main():
     df = pd.read_csv('dfout_all.csv', dtype={15: str})
     df = preprocess_data(df)
-    # plot_unique_users_per_month(df)
-    # plot_unique_users_per_year(df)
+    plot_unique_users_per_month(df)
+    plot_unique_users_per_year(df)
     plot_costs_per_year(df)
-
+    plot_costs_per_month(df)
 if __name__ == "__main__":
     main()
