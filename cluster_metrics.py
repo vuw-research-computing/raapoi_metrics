e2576e49b73f02ef38437c231d1e9ca72a1be856
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2023-07-12 13:22:46 +1200

    Let's use input arguments to decide what to run

--- cluster_metrics.py
+++ cluster_metrics.py
@@ -1,8 +1,27 @@
+import argparse
 from functions.process_collate import multiCollate
 from functions.slurm_data_collection import merge_slurm_data, grab_slurm_data, split_data_nprocs
 from functions.raapoi_plots import plot_all_slurm
 
+# Dictionary of available functions to run
+functions_dict = {
+    "multiCollate": multiCollate,
+    "merge_slurm_data": merge_slurm_data,
+    "grab_slurm_data": grab_slurm_data,
+    "split_data_nprocs": split_data_nprocs,
+    "plot_all_slurm": plot_all_slurm
+}
 
 if __name__ == '__main__':
-    # multiCollate()
-    plot_all_slurm()
+    # Create an ArgumentParser object
+    parser = argparse.ArgumentParser(description="Run specified function")
+
+    # Add a positional command line argument for the function name
+    parser.add_argument("function", choices=functions_dict.keys(), help="Name of the function to run")
+
+    # Parse command line arguments
+    args = parser.parse_args()
+
+    # Run the specified function
+    function_to_run = functions_dict[args.function]
+    function_to_run()
