d484f1e8fd2a04a15e7259df4b8484ed4d81bfaa
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2023-07-12 13:18:49 +1200

    Moved splitting the data to nprocs to slurm_data_collection

--- cluster_metrics.py
+++ cluster_metrics.py
@@ -1,7 +1,7 @@
-import pandas as pd
 from functions.process_collate import multiCollate
-from functions.slurm_data_collection import merge_slurm_data, grab_slurm_data
+from functions.slurm_data_collection import merge_slurm_data, grab_slurm_data, split_data_nprocs
 from functions.raapoi_plots import plot_all_slurm
+
 
 if __name__ == '__main__':
     # multiCollate()
