44ebee71bbd252983ad45f6fd97230db262a8c4e
Rohit Duggal <duggalrohi@cucina-giardino.ecs.vuw.ac.nz>
2025-09-17 13:05:15 +1200

    Fix error in cluster_price
    Create a final csv with headers

--- functions/cluster_price.py
+++ functions/cluster_price.py
@@ -31,6 +31,15 @@ def cleanjobid(jobid):
     jobid = jobid.split('.', 1)[0]
     return jobid
 
+# Fix non-numeric NTasks
+def fix_ntasks(val):
+    if pd.isnull(val):
+        return 0
+    try:
+        # Remove commas or stray whitespace and convert to int
+        return int(str(val).replace(',', '').strip())
+    except:
+        return 0  # Fallback for badly formatted strings
 
 def collate_saact(jobs_data, aws_cost_data):
     # Preprocessing steps from collate_saact function
@@ -49,6 +58,7 @@ def collate_saact(jobs_data, aws_cost_data):
     jobs_data.loc[:, 'TotalCPU'] = jobs_data['TotalCPU'].map(lambda x: timeformat_lambda(x))
 
     jobs_data.loc[:, 'JobID'] = jobs_data['JobID'].map(lambda x: cleanjobid(x))
+    jobs_data.loc[:, 'NTasks'] = jobs_data['NTasks'].map(lambda x: fix_ntasks(x))
 
 
     # Aggregating and processing as in the original collate_saact function
@@ -56,21 +66,23 @@ def collate_saact(jobs_data, aws_cost_data):
         'User': lambda x: x.iloc[0],
         'Account': lambda x: x.iloc[0],
         'JobID': lambda x: x.iloc[0],
-        'Elapsed': np.max,
-        'Timelimit': np.max,
+        'Elapsed': 'max',
+        'Timelimit': 'max',
         'Start': lambda x: x.iloc[0],
         'NNodes': lambda x: x.iloc[0],
-        'NTasks': np.max,
-        'MaxRSS': np.max,
-        'MaxVMSize': np.max,
+        'NTasks': 'max',
+        'MaxRSS': 'max',
+        'MaxVMSize': 'max',
         'Partition': lambda x: x.iloc[0],
         'ReqCPUS': lambda x: x.iloc[0],
         'AllocCPUS': lambda x: x.iloc[0],
-        'TotalCPU': np.max,
+        'TotalCPU': 'max',
         'ReqMem': lambda x: x.iloc[0],
         'State': lambda x: x.iloc[0],
-        'End': lambda x: x.iloc[0]
+        'End': lambda x: x.iloc[0],
+        'Submit': lambda x: x.iloc[0],
     })
+
     if not df_agg.empty:
         # aws_cost_data = prepare_aws_cost_data()
         df_agg[['aws_cost', 'nesi_cost']] = df_agg.apply(lambda row: aws_cost_equiv(row, aws_cost_data), axis=1)
